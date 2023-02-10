42439d3eeed3ef4e5c051919cceeff1321bfc428
seunghunbang <upi21x@gmail.com>
2023-02-10 15:52:19 +0900

    coinchart content UI setup done without functionalities of purchasing and selling and graph

--- src/components/ModalPopup/ModalPopup.js
+++ src/components/ModalPopup/ModalPopup.js
@@ -1,7 +1,138 @@
 import classes from "./ModalPopup.module.css";
 import CoinChart from "../CoinChart.js/CoinChart";
 import { AiFillCloseSquare } from "react-icons/ai";
+import { useRef } from "react";
+import axios from "axios";
 function ModalPopup({ title, show, close, index, data }) {
+  const inputSell = useRef();
+  const inputBuy = useRef();
+
+  const handleAdd1000k = (event) => {
+    event.preventDefault();
+    const buttonId = event.target.id;
+    if (buttonId === "purchase_add") {
+      if (!isNaN(inputBuy.current.value)) {
+        let numbers = parseInt(inputBuy.current.value);
+        numbers += +1000000;
+        // console.log(typeof numbers);
+        inputBuy.current.value = numbers;
+        // console.log(inputSell.current.value);
+      } else {
+        inputBuy.current.value = +1000000;
+      }
+    } else {
+      if (!isNaN(inputSell.current.value)) {
+        let numbers = parseInt(inputSell.current.value);
+        numbers += +1000000;
+        // console.log(typeof numbers);
+        inputSell.current.value = numbers;
+        // console.log(inputSell.current.value);
+      } else {
+        inputSell.current.value = +1000000;
+      }
+    }
+    return;
+  };
+
+  const handleAdd100k = (event) => {
+    event.preventDefault();
+    const buttonId = event.target.id;
+    if (buttonId === "purchase_add") {
+      if (!isNaN(inputBuy.current.value)) {
+        let numbers = parseInt(inputBuy.current.value);
+        numbers += +100000;
+        // console.log(typeof numbers);
+        inputBuy.current.value = numbers;
+        // console.log(inputSell.current.value);
+      } else {
+        inputBuy.current.value = +100000;
+      }
+    } else {
+      if (!isNaN(inputSell.current.value)) {
+        let numbers = parseInt(inputSell.current.value);
+        numbers += +100000;
+        // console.log(typeof numbers);
+        inputSell.current.value = numbers;
+        // console.log(inputSell.current.value);
+      } else {
+        inputSell.current.value = +100000;
+      }
+    }
+    return;
+  };
+  const handleAdd10k = (event) => {
+    event.preventDefault();
+    const buttonId = event.target.id;
+    if (buttonId === "purchase_add") {
+      if (!isNaN(inputBuy.current.value)) {
+        let numbers = parseInt(inputBuy.current.value);
+        numbers += +10000;
+        // console.log(typeof numbers);
+        inputBuy.current.value = numbers;
+        // console.log(inputSell.current.value);
+      } else {
+        inputBuy.current.value = +10000;
+      }
+    } else {
+      if (!isNaN(inputSell.current.value)) {
+        let numbers = parseInt(inputSell.current.value);
+        numbers += +10000;
+        // console.log(typeof numbers);
+        inputSell.current.value = numbers;
+        // console.log(inputSell.current.value);
+      } else {
+        inputSell.current.value = +10000;
+      }
+    }
+    return;
+  };
+
+  const handleBuy = (event) => {
+    event.preventDefault();
+    const buyAmount = inputBuy.current.value;
+    // const buyPrice = data.quote.KRW.price;
+    // const buyTotal = buyAmount * buyPrice;
+
+    axios
+      .post("example.com", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          buyAmount: buyAmount,
+        }),
+      })
+      .then((response) => {
+        console.log(response, "BUY RESPONSE");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleSell = (event) => {
+    event.preventDefault();
+    const sellAmount = inputSell.current.value;
+    // const sellPrice = data.quote.KRW.price;
+    // const sellTotal = sellAmount * sellPrice;
+
+    axios
+      .post("example.com", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          sellAmount: sellAmount,
+        }),
+      })
+      .then((response) => {
+        console.log(response, "SELL RESPONSE");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   if (show === null) {
     return null;
   } else if (show === index) {
@@ -25,16 +156,41 @@ function ModalPopup({ title, show, close, index, data }) {
               </div>
               <div className={classes.modal_popup_buy_sell_form}>
                 <input
+                  ref={inputBuy}
+                  min="0"
                   className={classes.modal_popup_buy_sell_form_input}
-                  type="text"
+                  defaultValue="0"
+                  type="number"
                   id="purchase"
                   placeholder="PURCHASE (KRW)"
                 />
                 <button
+                  onClick={handleBuy}
                   className={classes.modal_popup_buy_sell_form_button}
                   type="submit"
                 >
                   PURCHASE
+                </button>
+                <button
+                  id="purchase_add"
+                  onClick={handleAdd10k}
+                  className={classes.modal_popup_buy_sell_form_button}
+                >
+                  + 10,000 ₩
+                </button>
+                <button
+                  id="purchase_add"
+                  onClick={handleAdd100k}
+                  className={classes.modal_popup_buy_sell_form_button}
+                >
+                  + 100,000 ₩
+                </button>
+                <button
+                  id="purchase_add"
+                  onClick={handleAdd1000k}
+                  className={classes.modal_popup_buy_sell_form_button}
+                >
+                  + 1,000,000 ₩
                 </button>
               </div>
             </div>
@@ -55,16 +211,41 @@ function ModalPopup({ title, show, close, index, data }) {
 
               <div className={classes.modal_popup_buy_sell_form}>
                 <input
+                  ref={inputSell}
+                  min="0"
                   className={classes.modal_popup_buy_sell_form_input}
-                  type="text"
+                  type="number"
                   id="sell"
+                  defaultValue="0"
                   placeholder="SELL (KRW)"
                 />
                 <button
+                  onClick={handleSell}
                   className={classes.modal_popup_buy_sell_form_button}
                   type="submit"
                 >
                   SELL
+                </button>
+                <button
+                  id="sell_add"
+                  onClick={handleAdd10k}
+                  className={classes.modal_popup_buy_sell_form_button}
+                >
+                  + 10,000 ₩
+                </button>
+                <button
+                  id="sell_add"
+                  onClick={handleAdd100k}
+                  className={classes.modal_popup_buy_sell_form_button}
+                >
+                  + 100,000 ₩
+                </button>
+                <button
+                  id="sell_add"
+                  onClick={handleAdd1000k}
+                  className={classes.modal_popup_buy_sell_form_button}
+                >
+                  + 1,000,000 ₩
                 </button>
               </div>
             </div>
--- src/pages/users/ContactUsPage/ContactUsPage.js
+++ src/pages/users/ContactUsPage/ContactUsPage.js
@@ -1,7 +1,39 @@
+import classes from "./ContactUsPage.module.css";
+import { AiFillGithub, AiFillLinkedin, AiFillInstagram } from "react-icons/ai";
 function ContactUsPage() {
   return (
-    <div>
-      <p>This is Contact Us Page 이곳은 연락 페이지 관련자에게 연락</p>
+    <div className={classes.contact_container}>
+      <div className={classes.contact_sub_container}>
+        <h1 className={classes.contact_title}> DUMMY TITLE</h1>
+        <p className={classes.contact_description}> DUMMY DESCRIPTION</p>
+        <div>
+          {/* Classnames should be defined properly */}
+          <div className={classes.contact_icons}>
+            <a
+              href="https://github.com/a1603169"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <AiFillGithub className={classes.contact_icon} />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/bang-seunghun-18a465204/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <AiFillLinkedin className={classes.contact_icon} />
+            </a>
+            <a
+              href="https://www.instagram.com/seunghun_bang/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <AiFillInstagram className={classes.contact_icon} />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className={classes.contact_sub_container}>HELLLOOO</div>
     </div>
   );
 }

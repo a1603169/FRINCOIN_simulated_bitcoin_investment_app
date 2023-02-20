e5c5aa91a28aa7c18894cb4f3e709306b06b03fd
seunghunbang <upi21x@gmail.com>
2023-02-20 18:31:52 +0900

    coin chart basic rechart imported

--- src/components/CoinChart.js/CoinChart.js
+++ src/components/CoinChart.js/CoinChart.js
@@ -1,85 +1,30 @@
+import { useState, useEffect } from "react";
+import { LineChart, Line, XAxis, YAxis, Tooltip } from "recharts";
+
 function CoinChart() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.bithumb.com/public/ticker/BTC_KRW")
+      .then((response) => response.json())
+      .then((data) => {
+        // 데이터를 배열로 변환하여 최근 10개의 데이터 포인트만 유지
+        const chartData = Object.entries(data.data)
+          .map(([key, value]) => ({ time: key, price: value.closing_price }))
+          .slice(0, 10);
+        setData(chartData);
+        console.log(chartData);
+      });
+    console.log(data);
+  }, []);
+
   return (
-    <div>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-      <p>This is a chart</p>
-    </div>
+    <LineChart width={600} height={300} data={data}>
+      <XAxis dataKey="time" />
+      <YAxis />
+      <Tooltip />
+      <Line type="monotone" dataKey="price" stroke="#8884d8" />
+    </LineChart>
   );
 }
 
